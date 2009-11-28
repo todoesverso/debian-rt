f8375b667242ec9a668ede7a74d479277f662319
Santiago Bruno <bananabruno@gmail.com>
2009-11-28 16:12:53 -0300

    Using apt module instead of os.system('aptitude')
    Signed-off-by: todoesverso <todoesverso@gmail.com>

--- src/install_software.py
+++ src/install_software.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 #########################################################################
 # This file is part of debian-rt.                                       #
@@ -17,22 +18,41 @@
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.       #
 #########################################################################
 
+import apt
 import os
 import csv
 
-apps = []
-
+cache = apt.Cache()
 reader = csv.DictReader(open('programas_disponibles.csv'))
 
 for entry in reader:
     desc = entry['description']
     name = entry['name']
-    option = raw_input("Instalar %s ? (s/n): " % desc)
-    if option.lower() == 's':
-        apps.append(name)
+    if not cache.has_key(name):
+        print "Advertencia: el paquete %s (%s) no se encuentra en la cache \
+de apt. Ignorando." % (name, desc)
+        continue
+    package = cache[name]
+    if package.isInstalled and not package.isUpgradable:
+        print "El paquete %s ya se encuentra instalado y actualizado. \
+Ignorando" % name
+        continue
+    elif package.isInstalled and package.isUpgradable:
+        print "El paquete %s (%s) se encuentra instalado en su versión %s.\n" \
+              "La versión %s se encuentra disponible." % \
+        (name, desc, package.installed.version, package.candidate.version)
+        option = raw_input("Actualizar? (s/n): ")
+        if option.lower() == 's':
+            package.markUpgrade()
+    else:
+        option = raw_input("Instalar %s ? (s/n): " % desc)
+        if option.lower() == 's':
+            package.markInstall()
 
 print "\n------------------------------------\n\n"
 
 option = raw_input("Confirma instalar lo seleccionado? (s/n): ")
 if option.lower() == 's':
-   os.system("aptitude install " + " ".join(apps))
+    cache.commit(apt.progress.TextFetchProgress(),
+                 apt.progress.InstallProgress())
+
